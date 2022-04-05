d806cd7b49d435241ca46780ef5e20d1ca2f4723
debashishdas007 <50047193+debashishdas007@users.noreply.github.com>
2022-04-05 14:57:22 +0530

    Update business-service.js
    test in Origin change

--- srv/business-service.js
+++ srv/business-service.js
@@ -5,5 +5,6 @@ module.exports = cds.service.impl(async function() {
         return bp.run(req.query);   
         // const query = req.query        
         // return bp.emit ({ query, headers: { APIKey: process.env.APIkey } })    
+        // New changes
     });
 });
